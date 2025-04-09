a1bcf878bd56def9e4c686872cd23e5bcef6aede
sayooj <sayoojsathyan098@gmail.com>
2025-04-09 19:07:36 +0530

    created the Header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import Profile from './Pages/Profile'
+import About from './Pages/About'
+import SignIn from './Pages/SignIn'
+import SignOut from './Pages/SignOut'
+import Header from './Components/Header'
 function App() {
   return (
     <div>
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-out' element={<SignOut/>}/>
+
+
+
+      </Routes>
+      </BrowserRouter>
       
     </div>
   )
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 function Header() {
   return (
-    <div>
-      
+    <div className='bg-gray-300 '>
+      <div className=' h-[50px] flex justify-between items-center
+      max-w-6xl mx-auto'>
+        <Link to='/'>
+        <h1 className='font-semibold text-xl'>User Managment</h1>
+        </Link>
+        <ul className='flex gap-4'>
+          <Link to='/'>
+          <li>Home</li>
+          </Link>
+          <Link to='/about'>
+          <li>About</li>
+          </Link>
+          <Link to='/sign-in'>
+          <li>Sign in</li>
+          </Link>
+        </ul>
+      </div>
     </div>
   )
 }
