bca0648347c066b24c4599941992d3dd3ca18cc6
sayooj <sayoojsathyan098@gmail.com>
2025-04-10 00:35:22 +0530

    create user model

---
+++ models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { type } from "os";
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
